c0d2c27bb8754e5ab3b05fad6a8b1c939560bc40
MarySPerez <ms.perezgomez@gmail.com>
2025-04-13 18:17:08 -0400

    Se agrega el componente NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import AboutUs from "./pages/aboutUs"
 import Menu from "./components/Menu"
 import OurServices from "./pages/OurServices"
 import RegistrationForm from "./pages/registrationForm"
+import Navbar from "./components/NavBar"
 function App() {
   return (
     <>
       {/* <Menu /> */}
       {/* <AboutUs /> */}
       {/* <OurServices /> */}
+
+      <Navbar />
       <RegistrationForm />
       <Footer />
     </>
