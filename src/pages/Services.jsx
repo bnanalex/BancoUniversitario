ac88a1b4f135d266494ab182b90e5878815e51fd
MarySPerez <ms.perezgomez@gmail.com>
2025-04-18 12:01:26 -0400

    Implementacion de Foto Servicios

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Navbar from "../components/NavBar";
-import HeroSection from "../components/HeroSectionServices";
 import OurServices from "../components/OurServices";
 import Footer from "../components/footer";
 import HeroSectionServices from "../components/HeroSectionServices";
+import FotoServices from "../assets/images/services/services.jpg";
 
 const Services = () => {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 mb-20">
       {/* Header */}
       <Navbar />
       
@@ -23,9 +23,17 @@ const Services = () => {
           <OurServices />
         </section>
 
-        {/* Sección Principal de Servicios */}
-        <section className="mb-20 px-4 md:px-8 lg:px-16">
-          <OurServices />
+        {/* Foto */}
+        <section className="mb-20">
+          <div className="flex justify-center items-center mb-10">
+            <img
+              src={FotoServices}
+              alt="Servicios"
+              className="w-full h-auto max-w-[1200px] object-cover rounded-lg shadow-xl"
+              loading="lazy"
+              decoding="async"
+            />
+          </div>
         </section>
 
         {/* Footer */}
