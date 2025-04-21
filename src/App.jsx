b4b46f3994acc0685cf6ac1f0b6dc04fde5225d1
MarySPerez <ms.perezgomez@gmail.com>
2025-04-21 14:17:20 -0400

    Modificaciones resposives del HeroSection

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Páginas
 import Home from "./pages/Home";
 import Form from "./pages/registrationForm";
+import Services from "./pages/Services";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* Página principal */}
-        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Home />} />
 
         {/* Página de Login/Registro */}
         <Route path="/login" element={<Form />} />
+
+        {/* Página de Servicio */}
+        <Route path="/" element={<Services />} />
       </Routes>
     </Router>
   );
--- src/components/HeroSectionHome.jsx
+++ src/components/HeroSectionHome.jsx
@@ -1,22 +1,28 @@
 import Hero from '../assets/images/home/foto home proyecto lab II (1).png';
- 
- const HeroSectionHome = () => {
-   return (
-     <section className="bg-gray-50 box-border py-[25px] px-[0%] [@media(max-width:1200px)]:py-[20px] [@media(max-width:1200px)]:px-[0%] [@media(max-width:768px)]:py-[15px] [@media(max-width:768px)]:px-[0px] [@media(max-width:480px)]:py-[10px] [@media(max-width:480px)]:px-[0px]">
-       <div className="flex flex-col items-center max-w-[1200px] mx-auto">
-         <picture className="flex flex-col items-center w-full">
-           <source srcSet={Hero} type="image/png" />
-           <img
-             src={Hero}
-             alt="imagen"
-             className="w-full h-auto max-w-[4616px] object-contain [@media(max-width:768px)]:max-w-full"
-             loading="lazy"
-             decoding="async"
-           />
-         </picture>
-       </div>
-     </section>
-   );
- };
- 
- export default HeroSectionHome;
+
+const HeroSectionHome = () => {
+  return (
+    <section className="bg-gray-50 box-border pt-[60px] pb-[25px] px-0 
+      md:pt-[40px] 
+      sm:pt-[30px] 
+      xs:pt-[20px]">
+      
+      <div className="flex flex-col items-center max-w-[1200px] mx-auto">
+        <picture className="flex flex-col items-center w-full">
+          <img
+            src={Hero}
+            alt="imagen"
+            className="w-full h-auto max-w-full object-contain"
+            loading="lazy"
+            decoding="async"
+          />
+        </picture>
+      </div>
+
+      {/* Franja verde */}
+      <div className="h-2 w-full" style={{ backgroundColor: '#085F63' }}></div>
+    </section>
+  );
+};
+
+export default HeroSectionHome;
