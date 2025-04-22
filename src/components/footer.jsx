21e70b3de05e3e3dd531da9b9e4679056dce8547
MarySPerez <ms.perezgomez@gmail.com>
2025-04-21 23:25:03 -0400

    Agregue el twitter que faltaba en el footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../assets/images/home/Logo letras blancas.png';
 import facebookIcon from '../assets/icons/facebook-brands.svg';
 import instagramIcon from '../assets/icons/instagram-brands.svg';
-
+import twitterIcon from '../assets/icons/square-x-twitter-brands.svg';
 function Footer() {
   return (
     <footer className="bg-teal-800 text-white py-8">
@@ -27,8 +27,12 @@ function Footer() {
             <img src={facebookIcon} alt="Facebook" className="h-5 mr-1" />
             @bancouniversitariove
           </a>
-          <a href="https://www.instagram.com/bancouniversitariove" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center md:justify-start">
+          <a href="https://www.instagram.com/bancouniversitariove" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center md:justify-start mb-2 md:mb-2">
             <img src={instagramIcon} alt="Instagram" className="h-5 mr-1" />
+            @bancouniversitariove
+          </a>
+          <a href="https://x.com/home?lang=en" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center md:justify-start">
+            <img src={twitterIcon} alt="Twitter-X" className="h-5 mr-1" />
             @bancouniversitariove
           </a>
         </div>
