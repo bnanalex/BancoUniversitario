1147a5b8a664a2cdee616ee9da101e4acb2d9955
unknown <alexisacen@gmail.com>
2025-04-21 22:16:25 -0400

    arreglando conflictos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,14 @@
 import React from "react"
-<<<<<<< HEAD
 import HeroSection from "../components/HeroSectionHome"
 import ServicesSection from "../components/ServiceSection"
 import AboutUs from "../components/aboutUs" ;
 import Ob from "../components/Objetives"
 import MisionVision from "../components/MisionVision";
 import OurServices from "../components/OurServices"
-=======
-import Navbar from "../components/NavBar"   
-import HeroSection from "../components/Hero"
-import Ob from "../components/Objetives"
-import ServicesSection from "../components/ServicesSection"
-import MisionVision from "../components/MisionVision"
-import Footer from "../components/footer"   
->>>>>>> 674f30eb948b86bd20a6f13ad3783403dcda4072
 const Home = () => {
     return (
       <div className="min-h-screen bg-gray-50">
         {/* Header */}
-<<<<<<< HEAD
-=======
-        <Navbar />
->>>>>>> 674f30eb948b86bd20a6f13ad3783403dcda4072
         
         {/* Contenido principal */}
         <main className="max-w-7xl mx-auto sm:px-6 lg:px-5 py-12">
@@ -39,22 +26,10 @@ const Home = () => {
           {/* Sección Servicios */}
           {/* <section className="mb-5">
             <OurServices />
-<<<<<<< HEAD
           </section> */}
 
           {/* Sección Sobre Nosotros */}
           <section className="mb-20">
-=======
-          </section>
-
-          {/* Sección Mision y Vision */}
-          <section className="mb-5">
-            <MisionVision />
-          </section>
-
-          {/* Sección Servicios */}
-          <section className="mb-5">
->>>>>>> 674f30eb948b86bd20a6f13ad3783403dcda4072
             <AboutUs />
           </section>
   
@@ -67,21 +42,9 @@ const Home = () => {
             <section className="mb-20">
             <MisionVision/>
           </section>
-<<<<<<< HEAD
         </main>
       </div>
     );
   };
   
-=======
-  
-          {/* Sección Contacto */}
-          <section className="mb-5">
-            <Footer />
-          </section>
-          </main>
-          </div>
-    )
-}
->>>>>>> 674f30eb948b86bd20a6f13ad3783403dcda4072
   export default Home;
