7597a1a7bb0519e38e2da1e9ee33c974e85089d7
Luis Ochoa <luisarturo8a8a@gmail.com>
2025-05-26 22:55:14 -0400

    Creado el componente Comprobante con su respectivo diseño de interfaz

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import ContactsMenu from "../components/ContactsMenu"
 import ContactInfo from "../components/ContactInfo"
 import DeleteContactDialog from "../components/DeleteContactDialog"
 import DeletedContact from "../components/DeletedContact"
+import Comprobante from "../components/Comprobante"
 import AboutUs from "../components/aboutUs" ;
 import Ob from "../components/Objetives"
 import MisionVision from "../components/MisionVision";
@@ -29,29 +30,34 @@ const Home = () => {
           </section>
 
           {/* Sección Caja Saldo (Debe ir dentro de la Banca en linea) */}
-          {/* <section className="mb-0">
+          {/*<section className="mb-0">
             <CajaSaldo />
-          </section> */}
+          </section>*/}
 
           {/* Menú de Contactos (Debe ser accesible desde el menú lateral al iniciar sesión) */}
-          {/* <section className="mb-5">
+          {/*<section className="mb-5">
             <ContactsMenu />
           </section > */}
 
           {/* Información del Contacto (Debe ser accesible desde el menú de contactos al seleccionar el contacto y hacer clic en "ver contacto") */}
-          {/* <section className="mb-5">
+          {/*<section className="mb-5">
             <ContactInfo />
-          </section > */}
+          </section >*/}
 
           {/* Menú de Eliminar Contacto (Debe ser accesible desde el menú de contactos al seleccionar el contacto y hacer clic en "eliminar") */}
-          {/* <section className="mb-5">
+          {/*<section className="mb-5">
             <DeleteContactDialog />
-          </section > */}
+          </section >*/}
 
           {/* Menú de Contacto Eliminado (Debe ser accesible desde el menú de Elinimar contacto al hacer clic en "Aceptar") */}
-          {/* <section className="mb-5">
+          {/*<section className="mb-5">
             <DeletedContact />
-          </section > */}
+          </section >*/}
+
+          {/* Menú de Comprobante (Debe ser accesible desde la seccion de movimientos dentro de la banca en linea, al hacer clic en "Ver Más" en un determinado movimiento) */}
+          {/*<section className="mb-5">
+            <Comprobante />
+          </section >*/}
 
           {/* Sección Sobre Nosotros */}
           <section className="mb-20">
